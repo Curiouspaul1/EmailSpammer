151fe833269fc749ae3e4104b41f615471d35fb3
Curiouspaul1 <170408025@live.unilag.edu.ng>
2019-07-18 22:31:11 +0100

    Added cron mail sending functionality

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+
+'''heroku link
+https://pure-savannah-79098.herokuapp.com'''
+
 #Dependencies
 from flask import Flask,request,jsonify
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_marshmallow import Marshmallow
 #from config import uri
-from smtplib import SMTP
-
-# instantiate smtpObject
-smptpObj = SMTP()
+from flask_mail import Mail, Message
+import random
+import time
 
 #instantiate flask app
 app = Flask(__name__)
+
+# Mail Server COnfig
+
+app.config.update(dict(
+    DEBUG = True,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = 'paulcurious7@gmail.com',
+    MAIL_PASSWORD = 'mechatronic',
+))
+
+
+mail = Mail(app)
 
 #configurations
 #app.config['SQLALCHEMY_DATABASE_URI'] = uri
@@ -39,6 +57,29 @@ class user_dataSchema(ma.Schema):
 userSchema = user_dataSchema(strict=True)  # single user object schema
 users_Schema = user_dataSchema(many=True,strict=True) # multiple user object schena
 '''
+@app.route('/')
+def index():
+	'''msg = Message('',sender='paulcurious7@gmail.com',recipients=['intheknoow@gmail.com'])
+	text = 
+			An electromagnetic pulse (EMP), also sometimes called a transient electromagnetic disturbance, is a short burst of electromagnetic energy. Such a pulse may occur in the form of a radiated electric or magnetic field or conducted electrical current depending on the source. EMP Jammer is a device capable of generating a transient electromagnetic disturbance that radiates outward from its epicenter, disrupting electronic devices
+	for i in range(5):
+		text = text.split()
+		random.shuffle(text)
+		text = ''.join(i + ' ' for i in text)
+		msg.body = text
+		mail.send(msg)
+		time.sleep(60)'''
+	for i in range(5):
+		text = '''
+			An electromagnetic pulse (EMP), also sometimes called a transient electromagnetic disturbance, is a short burst of electromagnetic energy. Such a pulse may occur in the form of a radiated electric or magnetic field or conducted electrical current depending on the source. EMP Jammer '''
+		text = text.split()
+		random.shuffle(text)
+		text = ''.join(i + ' ' for i in text)
+		msg = Message('',sender='paulcurious7@gmail.com',recipients=['intheknoow@gmail.com'])
+		msg.body = text
+		mail.send(msg)
+		time.sleep(10)
+	return 'Message sent'
 @app.route('/rec',methods=['GET','POST'])
 def rec():
 	#Get Data
@@ -56,3 +97,4 @@ def rec():
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
