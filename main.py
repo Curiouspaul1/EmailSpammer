c6578321a1096e65b868e00196b93d72d6d42e8b
Curiouspaul1 <170408025@live.unilag.edu.ng>
2019-07-18 22:56:08 +0100

    Added more dummy text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 https://pure-savannah-79098.herokuapp.com'''
 
 #Dependencies
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,render_template
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_marshmallow import Marshmallow
 #from config import uri
@@ -59,41 +59,43 @@ users_Schema = user_dataSchema(many=True,strict=True) # multiple user object sch
 '''
 @app.route('/')
 def index():
-	'''msg = Message('',sender='paulcurious7@gmail.com',recipients=['intheknoow@gmail.com'])
-	text = 
-			An electromagnetic pulse (EMP), also sometimes called a transient electromagnetic disturbance, is a short burst of electromagnetic energy. Such a pulse may occur in the form of a radiated electric or magnetic field or conducted electrical current depending on the source. EMP Jammer is a device capable of generating a transient electromagnetic disturbance that radiates outward from its epicenter, disrupting electronic devices
-	for i in range(5):
-		text = text.split()
-		random.shuffle(text)
-		text = ''.join(i + ' ' for i in text)
-		msg.body = text
-		mail.send(msg)
-		time.sleep(60)'''
+	return render_template('index.html')
+
+@app.route('/sendmail',methods=['GET','POST'])
+def sendmail():
+	#Get Data
+	email = request.form['email']
+	frequency = request.form['frequency']
+	frequency = abs(eval(frequency))
+
 	for i in range(5):
 		text = '''
-			An electromagnetic pulse (EMP), also sometimes called a transient electromagnetic disturbance, is a short burst of electromagnetic energy. Such a pulse may occur in the form of a radiated electric or magnetic field or conducted electrical current depending on the source. EMP Jammer '''
+			 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum nisi at turpis pellentesque, ac dapibus augue eleifend. Nunc quis felis quis lorem iaculis dictum. Integer sit amet ultricies quam. Nam vel rhoncus arcu, vitae auctor ex. Phasellus lorem quam, feugiat aliquet rutrum id, pharetra vel tortor. Pellentesque sagittis est at suscipit rutrum. Nulla quis tempor felis. Sed eu tellus feugiat, sagittis elit ut, tincidunt ante. Aenean id elit justo. Ut consectetur luctus nunc ac mattis. Nullam at pharetra arcu. Aenean sodales nunc sed fringilla blandit. Suspendisse pretium mi at urna aliquam, non luctus enim congue. Duis vehicula, enim efficitur hendrerit posuere, mauris neque malesuada massa, venenatis blandit mi tellus in dolor. Aliquam erat volutpat.
+
+Proin accumsan mi eu tempus mollis. Morbi id sodales quam, eu blandit leo. Nam convallis, nibh et semper feugiat, turpis dui pharetra mi, nec ultricies arcu lorem ac nibh. Curabitur sit amet purus id justo posuere mollis ut at sapien. Integer semper nec erat tincidunt tempus. Nunc mi velit, malesuada quis sapien id, porta hendrerit magna. Integer vel auctor libero. Vivamus ornare convallis convallis. Morbi porta odio vitae ex sollicitudin, id elementum risus laoreet. Proin interdum odio eget velit ultricies eleifend. Morbi id massa pulvinar, lacinia tellus id, mattis elit. Nullam vehicula, nisi ut placerat tempus, lectus enim convallis odio, pretium hendrerit lectus lorem nec massa. Mauris dapibus, magna quis malesuada tristique, turpis justo semper lorem, vitae dapibus lacus tortor a nunc. Suspendisse pellentesque libero eget velit rhoncus, non rhoncus ipsum aliquet. In hendrerit blandit ligula dictum pharetra. Maecenas ligula justo, euismod sed orci semper, lobortis hendrerit eros.  '''
 		text = text.split()
 		random.shuffle(text)
 		text = ''.join(i + ' ' for i in text)
-		msg = Message('',sender='paulcurious7@gmail.com',recipients=['intheknoow@gmail.com'])
+		msg = Message('',sender='paulcurious7@gmail.com',recipients=[email])
 		msg.body = text
 		mail.send(msg)
-		time.sleep(10)
+		time.sleep(frequency)
 	return 'Message sent'
-@app.route('/rec',methods=['GET','POST'])
+
+'''@app.route('/rec',methods=['GET','POST'])
 def rec():
 	#Get Data
-	email = request.json['email']
-	frequency = request.json['frequency']
+	email = request.form['email']
+	frequency = request.form['frequency']
 
 	#create user_data
-	'''newData = user_data(email,frequency)
+	newData = user_data(email,frequency)
 
 	#Add to db
 	db.session.add(newData)
 	db.session.commit()
 
-	return userSchema.jsonify(newData)'''
+	return render_template('index.html')'''
 
 if __name__ == '__main__':
 	app.run(debug=True)
